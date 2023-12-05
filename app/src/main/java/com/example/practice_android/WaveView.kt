70b84809267ecb06a8b5ef840cb28f2de53cddbe
PCYSB <pcysb365@gmail.com>
2023-12-06 00:48:48 +0900

    웨이브뷰 테스트용으로 중간에 설정이 잘못되었던것 같았으나 jcenter() android.enableJetifier=true 를 추가하여 해결하였다

--- app/src/main/java/com/example/practice_android/WaveView.kt
+++ app/src/main/java/com/example/practice_android/WaveView.kt
@@ -1,36 +1,24 @@
 package com.example.practice_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.view.animation.LinearInterpolator
-import android.view.animation.TranslateAnimation
-import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.scwang.wave.MultiWaveHeader
+
 
 class WaveView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wave_view)
 
-        val waveImageView = findViewById<ImageView>(R.id.wave_anim)
-        val waveAnimation = createWaveAnimation()
-        waveImageView.startAnimation(waveAnimation)
+        var wave_header: MultiWaveHeader
+        wave_header=findViewById(R.id.wave_header);
+        wave_header.setVelocity(10F)
+        wave_header.setProgress(1F)
+        wave_header.isRunning()
+        wave_header.setGradientAngle(45)
+        wave_header.setWaveHeight(40)
+
+
     }
 
-    private fun createWaveAnimation(): Animation {
-        val animation = TranslateAnimation(
-            Animation.RELATIVE_TO_SELF, 0f,
-            Animation.RELATIVE_TO_SELF, 0f,
-            Animation.RELATIVE_TO_SELF, 0f,
-            Animation.RELATIVE_TO_SELF, 0.5f
-        )
-
-        animation.duration = 2000
-        animation.interpolator = LinearInterpolator()
-        animation.repeatCount = Animation.INFINITE
-        animation.repeatMode = Animation.REVERSE
-
-        return animation
-    }
 }
